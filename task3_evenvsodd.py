18bc0574d4489f22c09dc9b0c180683ab8cbf5e7
prakash <prakashpoint2005@gmail.com>
2026-02-10 20:33:30 +0530

    first commit

---
+++ task3_evenvsodd.py
@@ -0,0 +1,8 @@
+#Check even or odd number
+
+number = int(input("Enter a Number:"))
+
+if number % 2 == 0:
+    print(f"{number} is a even number")  
+else:
+    print(f"{number} is an odd number")
---
+++ task6_revstr.py
@@ -0,0 +1,6 @@
+def rev_string(s:str) -> str:
+    return s[::-1]
+
+str1=input("Enter the string to be reversed : ")
+
+print(f"reversed string : {rev_string(str1)}")
